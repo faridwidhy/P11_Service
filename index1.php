7f3fb1fd4f1f0ae2253f8e5633b317344c05bb8c
aliefezaa <89144143+aliefezaa@users.noreply.github.com>
2024-05-30 18:01:12 +0700

    Update index1.php

--- index1.php
+++ index1.php
@@ -1,4 +1,5 @@
 <?php
+// membuat method get dan menghubungkan ke database
 header('Content-Type: application/json; charset=utf8');
 
 $koneksi = mysqli_connect("localhost", "root", "", "toko_buku");
@@ -12,5 +13,34 @@ if($_SERVER['REQUEST_METHOD'] === 'GET'){
     }
     echo json_encode($array_data);
 }
+
+<<<<<<< HEAD
+<<<<<<< HEAD
+// membuat method create
+
+else if($_SERVER['REQUEST_METHOD'] === 'POST'){
+    $judul_buku = $_POST['judul_buku'];
+    $jumlah = $_POST['jumlah'];
+    $harga = $_POST['harga'];
+    $sql = "INSERT INTO stok_buku (judul_buku, jumlah, harga) VALUES ('$judul_buku','$jumlah','$harga')";
+    $cek = mysqli_query($koneksi, $sql);
+
+    $response = ['status' => $cek ? 'berhasil' : 'gagal'];
+    echo json_encode($response);
+}
+
+else if($_SERVER['REQUEST_METHOD'] === 'DELETE'){
+    parse_str(file_get_contents("php://input"), $_DELETE);
+    $id = $_DELETE['id'];
+    $sql = "DELETE FROM stok_buku WHERE id='$id'";
+    $cek = mysqli_query($koneksi, $sql);
+
+    $response = ['status' => $cek ? 'berhasil' : 'gagal'];
+    echo json_encode($response);
+}
+=======
+>>>>>>> 266c830105bf21748952a04ebc014b702534e76c
+=======
+>>>>>>> 266c830105bf21748952a04ebc014b702534e76c
 
 ?>
