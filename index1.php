51698e90214bf898573082fb98942f58976c7022
Farid Widhy Asee <95490135+widhyasee@users.noreply.github.com>
2024-05-30 17:40:02 +0700

    Update index1.php

--- index1.php
+++ index1.php
@@ -1,4 +1,16 @@
 <?php
+header('Content-Type: application/json; charset=utf8');
 
+$koneksi = mysqli_connect("localhost", "root", "", "toko_buku");
+
+if($_SERVER['REQUEST_METHOD'] === 'GET'){
+    $sql = "SELECT * FROM stok_buku";
+    $query = mysqli_query($koneksi, $sql);
+    $array_data = array();
+    while($data = mysqli_fetch_assoc($query)){
+        $array_data[] = $data;
+    }
+    echo json_encode($array_data);
+}
 
 ?>
