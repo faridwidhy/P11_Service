06115fb7711fa148fc63985a124ae631e21efe86
Farid Widhy Asee <95490135+widhyasee@users.noreply.github.com>
2024-05-30 21:39:05 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", function () {
+  fetch("xampp/htdocs/P11_service/index1.php")
+    .then((response) => response.json())
+    .then((data) => {
+      const bookList = document.getElementById("books");
+      data.forEach((book) => {
+        const li = document.createElement("li");
+        li.innerHTML = `<strong>${book.judul_buku}</strong> - Jumlah: ${book.jumlah} - Harga: ${book.harga}`;
+        bookList.appendChild(li);
+      });
+    })
+    .catch((error) => console.error("Error:", error));
+});
